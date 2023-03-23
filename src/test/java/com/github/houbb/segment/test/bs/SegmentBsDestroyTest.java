61d189fbed3499771e1f83963977b3a4b6e87970
binbin.hou <binbin.hou@huifu.com>
2023-03-23 11:27:22 +0800

    [update] updates

--- src/test/java/com/github/houbb/segment/test/bs/SegmentBsDestroyTest.java
+++ src/test/java/com/github/houbb/segment/test/bs/SegmentBsDestroyTest.java
@@ -23,11 +23,11 @@ public class SegmentBsDestroyTest {
         final SegmentBs segmentBs = SegmentBs.newInstance();
 
         final String string = "这是一个伸手不见五指的黑夜。我叫孙悟空，我爱北京，我爱学习。";
-        List<ISegmentResult> resultList = SegmentBs.newInstance().segment(string);
+        List<ISegmentResult> resultList = segmentBs.segment(string);
         Assert.assertEquals("[这是[0,2), 一个[2,4), 伸手不见五指[4,10), 的[10,11), 黑夜[11,13), 。[13,14), 我[14,15), 叫[15,16), 孙悟空[16,19), ，[19,20), 我爱[20,22), 北京[22,24), ，[24,25), 我爱[25,27), 学习[27,29), 。[29,30)]", resultList.toString());
 
         // 重新处理
-        List<ISegmentResult> resultList2 = SegmentBs.newInstance().segment(string);
+        List<ISegmentResult> resultList2 = segmentBs.segment(string);
         Assert.assertEquals("[这是[0,2), 一个[2,4), 伸手不见五指[4,10), 的[10,11), 黑夜[11,13), 。[13,14), 我[14,15), 叫[15,16), 孙悟空[16,19), ，[19,20), 我爱[20,22), 北京[22,24), ，[24,25), 我爱[25,27), 学习[27,29), 。[29,30)]", resultList2.toString());
     }
 
@@ -40,14 +40,14 @@ public class SegmentBsDestroyTest {
         final SegmentBs segmentBs = SegmentBs.newInstance();
 
         final String string = "这是一个伸手不见五指的黑夜。我叫孙悟空，我爱北京，我爱学习。";
-        List<ISegmentResult> resultList = SegmentBs.newInstance().segment(string);
+        List<ISegmentResult> resultList = segmentBs.segment(string);
         Assert.assertEquals("[这是[0,2), 一个[2,4), 伸手不见五指[4,10), 的[10,11), 黑夜[11,13), 。[13,14), 我[14,15), 叫[15,16), 孙悟空[16,19), ，[19,20), 我爱[20,22), 北京[22,24), ，[24,25), 我爱[25,27), 学习[27,29), 。[29,30)]", resultList.toString());
 
         // 资源释放
         segmentBs.destroy();
 
         // 重新处理
-        List<ISegmentResult> resultList2 = SegmentBs.newInstance().segment(string);
+        List<ISegmentResult> resultList2 = segmentBs.segment(string);
         Assert.assertEquals("[这是[0,2), 一个[2,4), 伸手不见五指[4,10), 的[10,11), 黑夜[11,13), 。[13,14), 我[14,15), 叫[15,16), 孙悟空[16,19), ，[19,20), 我爱[20,22), 北京[22,24), ，[24,25), 我爱[25,27), 学习[27,29), 。[29,30)]", resultList2.toString());
     }
 
